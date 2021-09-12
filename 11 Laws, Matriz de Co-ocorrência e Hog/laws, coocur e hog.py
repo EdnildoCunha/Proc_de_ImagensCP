9674e3086a616cba1d3717a3b1e96dbdefe08210
EdnildoCunha <ednildocunha@gmail.com>
2021-09-12 17:34:49 -0300

    Hog feito

---
+++ 11 Laws, Matriz de Co-ocorrência e Hog/laws, coocur e hog.py
@@ -0,0 +1,24 @@
+from skimage.io import imread, imshow
+from skimage.transform import resize
+from skimage.feature import hog
+import cv2
+
+
+def HOG(img):
+    fd, hog_image = hog(img, orientations=8, pixels_per_cell=(16, 16),
+                        cells_per_block=(1, 1), visualize=True, multichannel=True)
+    print(fd)
+    print(hog_image)
+
+    return hog_image
+
+
+filename = "img_b.jpg"
+
+# lê a imagem
+img_b = imread(filename, 0)
+
+# chama a função
+result_hog = HOG(img_b)
+
+cv2.imwrite("hog.jpg", result_hog)
