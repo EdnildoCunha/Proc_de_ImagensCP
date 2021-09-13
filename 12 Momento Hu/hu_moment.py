b66c63d9fa99aa7b75e1cc32e8035e1500e153e1
EdnildoCunha <ednildocunha@gmail.com>
2021-09-13 13:14:54 -0300

    HU MOMENTS

--- 12 Momento Hu/hu_moment.py
+++ 12 Momento Hu/hu_moment.py
@@ -1,11 +1,9 @@
 import cv2
-import numpy as np
 
 # Hu moments
 
 
 def Humoment(img):
-    img = np.uint8(img)
     result_img = cv2.HuMoments(cv2.moments(img)).flatten()
     return print(result_img)
 
